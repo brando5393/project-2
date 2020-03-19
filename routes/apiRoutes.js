abd563574dfafc719bdff2710c1b5d9ff9b8cf5f
klhelker <57188023+klhelker@users.noreply.github.com>
2020-03-18 21:51:05 -0500

    put API to nyt in apiRoutes.js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,3 +22,42 @@ module.exports = function(app) {
     });
   });
 };
+
+
+// var mvAPI = "gqwrl6BDOR2rKmQC0A9fgypbxgCazUrk";
+// // NYT testing API Key. the use of keyword search options technology and programming which means getting the latest list 
+var queryURL=`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=technology&fq=programming&api-key=gqwrl6BDOR2rKmQC0A9fgypbxgCazUrk`;
+console.log(queryURL);
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+
+.then(function(res) {
+
+  console.log(res.response.docs);
+
+  var articleArray = res.response.docs;
+
+  for( var i = 0; i < articleArray.length; i++) {
+
+  //   
+   console.log (articleArray[i]);
+  //   // 
+   var articleURL = articleArray[i].web_url;
+   var byline = articleArray[i].byline.original;
+   var pub_date = articleArray[i].pub_date;
+   var headline = articleArray[i].headline.main;
+   var paragraph = articleArray[i].lead_paragraph;
+   var source = articleArray[i].source;
+     
+   var results = $("<h5>").html("<div>" + articleURL + byline + pub_date + headline + paragraph + source + "</div>");
+  
+  // console.log(results);
+
+  $(".docs").append(results)
+}
+})
+   
+
