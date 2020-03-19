4ab0df480916b5f02192db5fd4a9ed9bacbfe002
Dylan Myers <dylanmyers178@gmail.com>
2020-03-18 20:54:20 -0500

    base code for routes

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,24 +1,31 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  //find all posts by a user
+ app.get("/api/users", function(req, res){
+   db.User.findAll({
+     include: [db.Post]
+   }).then(function(dbUser){
+     res.json(dbUser);
+   });
+ });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+ //create User
+ app.post("/api/users", function(req, res){
+   db.User.create(req.body).then(function(dbUser){
+     res.json(dbUser);
+   });
+ });
+
+ //Delete User
+ app.delete("/api/users/:id", function(req, res){
+   db.User.destroy({
+     where: {
+       id: req.params.id
+     }
+   }).then(function(dbUser){
+     res.json(dbUser);
+   });
+ });
 };
